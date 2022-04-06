322a8b6be803516920c752b577e91547601bd9c8
superstraw <75589110+superstraw@users.noreply.github.com>
2022-04-06 18:06:53 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,35 @@
+function plane(){
+    $("#plane")
+        .css('color', 'green')
+        .fadeOut(1000);
+}
+function duck(){
+    $("#duck")
+        .css('color', 'green')
+        .fadeOut(1000);
+}
+function apple(){
+    $("#apple")
+        .css('color', 'green')
+        .fadeOut(1000);
+}
+function birdhouse(){
+    $("#birdhouse")
+        .css('color', 'green')
+        .fadeOut(1000);
+}
+function kitten(){
+    $("#kitten")
+        .css('color', 'green')
+        .fadeOut(1000);
+}
+function zebra_jeep(){
+    $("#zebra_jeep")
+        .css('color', 'green')
+        .fadeOut(1000);
+}
+function fire_hydrant(){
+    $("#fire_hydrant")
+        .css('color', 'green')
+        .fadeOut(1000);
+}
